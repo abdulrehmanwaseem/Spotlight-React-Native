2764b70644760248ec3cbc15b4bbabf124afd65b
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2025-03-20 16:49:36 +0500

    Add tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,70 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "@/constants/theme";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.primary,
+        tabBarInactiveTintColor: COLORS.grey,
+        tabBarStyle: {
+          elevation: 0,
+          borderTopWidth: 0,
+          position: "absolute",
+          height: 55,
+          paddingBottom: 0,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="home" size={size + 2} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="bookmarks"
+        options={{
+          title: "Bookmarks",
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="bookmark" size={size + 2} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="add-circle" size={size + 5} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          title: "Notifications",
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="notifications" size={size + 3} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="person-circle" size={size + 3} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "../../styles/home.styles";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Visit Notifications Screen</Text>
+    </View>
+  );
+}
